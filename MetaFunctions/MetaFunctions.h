344457b144b5119a8cbdc61544bc4d03a59cb98a
Alexey Volkov <a.volkov@stc-brn.ru>
2024-01-28 19:44:50 +0300

    Добавление документации

--- MetaFunctions/MetaFunctions.h
+++ MetaFunctions/MetaFunctions.h
@@ -7,28 +7,69 @@
 #include <limits>
 #include <string>
 
+/*!
+\brief Метафункции для работы с контейнерами <i>std::vector</i>, <i>std::list</i>, <i>std::tuple</i>
+\author Алексей Волков
+\version 1.0
+\date Январь 2024
+
+Метафункци, которые определяют, является ли рассматриваемый тип <i>std::vector</i>, <i>std::list</i> или <i>std::tuple</i>. Есть возможность проверить соответствие типа элементов <i>std::tuple</i> целочисленным типам(<i>int8_t</i>, <i>uint8_t</i>, ...)
+*/
+
 namespace meta {
+/*!
+Проверяет является ли указанный тип <b>T</b> контейнером
+\param T проверяемый тип
+\return <i>true</i> для <i>std::vector</i>, <i>std::list</i>. <i>false</i> для остальных типов
+*/
 template<typename T>
 struct is_container : std::false_type {};
 
+/*!
+Перегрузка <i>is_container</i> для <i>std::list</i>
+\param T проверяемый тип
+\return <i>true</i> для <i>std::list</i>. <i>false</i> для остальных типов
+*/
 template <typename... Ts>
 struct is_container< std::list<Ts...> > : std::true_type{};
 
+/*!
+Перегрузка <i>is_container</i> для <i>std::vector</i>
+\param T проверяемый тип
+\return <i>true</i> для <i>std::vector</i>. <i>false</i> для остальных типов
+*/
 template <typename... Ts>
 struct is_container< std::vector<Ts...> > : std::true_type{};
 
 template <typename... Ts>
 inline constexpr bool is_container_v = is_container<Ts...>::value;
 
+/*!
+Проверяет является ли указанный тип <b>T</b> <i>std::tuple</i>
+\param T проверяемый тип
+\return <i>true</i> для <i>std::tuple</i>. <i>false</i> для остальных типов
+*/
 template<typename T>
 struct is_tuple : std::false_type {};
 
+/*!
+Перегрузка <i>is_tuple</i> для <i>std::tuple</i>
+\param T проверяемый тип
+\return <i>true</i> для <i>std::tuple</i>. <i>tuple</i> для остальных типов
+*/
 template <typename... Ts>
 struct is_tuple< std::tuple<Ts...> > : std::true_type {};
 
 template <typename... Ts>
 inline constexpr bool is_tuple_v = is_tuple<Ts...>::value;
 
+/*!
+Представляет <i>std::tuple</i> в виде строки из его элементов, соединенных символом <i>separator</i>
+\param Tuple тип <i>std::tuple</i>
+\param tuple <i>std::tuple</i>
+\param separator разделитель
+\return <i>std::string</i>
+*/
 template <typename Tuple, std::size_t I = 0>
 std::string tuple_to_string(const Tuple& tuple, const char& separator = '.') {
     if constexpr (I == std::tuple_size_v<Tuple> - 1) {
@@ -39,6 +80,10 @@ std::string tuple_to_string(const Tuple& tuple, const char& separator = '.') {
     }
 }
 
+/*!
+Проверяет на этапе компиляции являеются ли типы элементов <i>std::tuple</i> целочисленными
+\param Tuple тип <i>std::tuple</i>
+*/
 template <typename Tuple, std::size_t I = 0>
 void check_integer_tuple_type() {
     if constexpr (I < std::tuple_size_v<Tuple>) {
--- PrintIP/PrintIP.h
+++ PrintIP/PrintIP.h
@@ -4,13 +4,33 @@
 #include <MetaFunctions.h>
 #include <sstream>
 
+/*!
+\brief Функции парсинга и печати условного IP-адреса
+\author Алексей Волков
+\version 1.0
+\date Январь 2024
+
+Функци, которые которые парсят и печают на стандартный ввывод условный IP-адрес. Работают с целочисленными типами, <i>std::string</i>, <i>std::list</i>, <i>std::vector</i>, <i>std::tuple</i>
+*/
 namespace parse {
 
+/*!
+Прасит строку IP-адреса, строка возвращается неизменной
+\param ip строковый IP-адрес
+\param separator разделитель
+\return <i>std::string</i>
+*/
 template<typename String, std::enable_if_t<std::is_same_v<String, std::string>, bool> = true>
 std::string parse_ip(const String& ip, const char& separator = '.'){
     return ip;
 }
 
+/*!
+Прасит целочисленный IP-адрес, возращает строку из значений байтов, соединенных <i>separator</i>, начиная со старшего байта
+\param ip целочисленный IP-адрес
+\param separator разделитель
+\return <i>std::string</i>
+*/
 template<typename Integer, std::enable_if_t<std::numeric_limits<Integer>::is_integer, bool> = true>
 std::string parse_ip(const Integer& ip, const char& separator = '.'){
     std::make_unsigned_t<Integer> mask;
@@ -48,6 +68,12 @@ std::string parse_ip(const Integer& ip, const char& separator = '.'){
     return stream.str();
 }
 
+/*!
+Прасит контейнер <i>std::list</i> или <i>std::vector</i> с IP-адресом, возращает строку из значений контейнера, соединенных <i>separator</i>
+\param ip контейнер с IP-адресом
+\param separator разделитель
+\return <i>std::string</i>
+*/
 template<typename Container, std::enable_if_t<meta::is_container_v<Container>, bool> = true>
 std::string parse_ip(const Container& ip, const char& separator = '.'){
 
@@ -70,6 +96,13 @@ std::string parse_ip(const Container& ip, const char& separator = '.'){
     return stream.str();
 }
 
+/*!
+Прасит <i>std::tuple</i> с IP-адресом, возращает строку из значений <i>std::tuple</i>, соединенных <i>separator</i>
+\param ip контейнер с IP-адресом
+\param separator разделитель
+\warning Элементы <i>std::tuple</i> должны быть целочислеными, в противном случае будет ошибка на этапе компиляции
+\return <i>std::string</i>
+*/
 template<typename Tuple, std::enable_if_t<meta::is_tuple_v<Tuple>, bool> = true>
 std::string parse_ip(const Tuple& ip, const char& separator = '.') {
     if(std::tuple_size_v<Tuple> == 0) {
@@ -81,6 +114,11 @@ std::string parse_ip(const Tuple& ip, const char& separator = '.') {
     return meta::tuple_to_string<Tuple>(ip, separator);
 }
 
+/*!
+Печатает на стандартный вывод распарсенный IP-адрес
+\param ip IP-адрес
+\param separator разделитель
+*/
 template<typename T>
 void print_ip(const T& ip, const char& separator = '.') {
     std::cout << parse_ip(ip, separator) << std::endl;
